49c1cd834db708c35201bfbe4ab1b3d901f1002e
Jessica Barbosa <je_barbosaa@hotmail.com>
2017-04-24 22:27:17 -0300

    Edição da classe ContatoDAO

--- src/java/br/cesjf/lppo/dao/ContatoDAO.java
+++ src/java/br/cesjf/lppo/dao/ContatoDAO.java
@@ -16,12 +16,15 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ContatoDAO {
-        private PreparedStatement opListar;
-        private PreparedStatement opNovo;
+        private final PreparedStatement opListar;
+        private final PreparedStatement opNovo;
+        private final PreparedStatement opAtualiza;
         
         public ContatoDAO() throws Exception{
             Connection conexao = ConnectionFactory.createConnection();
                 opListar = conexao.prepareStatement("SELECT * FROM contato");
+                opNovo  = conexao.prepareStatement("INSERT INTO contato (nome, sobrenome, telefone) VALUES(?, ?, ?)");
+                opAtualiza = conexao.prepareStatement("UPDATE contato SET nome = ?, sobrenome = ?, telefone = ? WHERE id = ?");
                 
         }
     
@@ -31,7 +34,7 @@ public class ContatoDAO {
             
             
            
-            ResultSet resultado = opListar.executeQuery(" SELECT * FROM contato");
+            ResultSet resultado = opListar.executeQuery();
                 while(resultado.next()){
                     Contato novoContato = new Contato();
                     novoContato.setId(resultado.getLong("id"));
@@ -51,14 +54,30 @@ public class ContatoDAO {
 
     public void cria(Contato novoContato) throws Exception {
         try{
-        Connection conexao = ConnectionFactory.createConnection();
-        Statement operacao = conexao.createStatement();
-        operacao.executeUpdate("INSERT INTO contato(nome, sobrenome, telefone) VALUES('"
-                +novoContato.getNome()+ "','"
-                +novoContato.getSobrenome()+ "','"
-                +novoContato.getTelefone()+ "')");
-        }catch(ClassNotFoundException ex){
-            throw new Exception("Erro ao carregar o Driver", ex);
+            opNovo.clearParameters();
+        opNovo.setString(1, novoContato.getNome());
+        opNovo.setString(2, novoContato.getSobrenome());
+        opNovo.setString(3, novoContato.getTelefone());
+        
+        opNovo.executeUpdate();
+        
+        }catch (SQLException ex){
+            throw new Exception("Erro ao inserir o contato", ex);
+        }
+    }
+        public void atualiza(Contato contato) throws Exception {
+        try{
+            
+        
+        opAtualiza.clearParameters();
+        opAtualiza.setString(1, contato.getNome());
+        opAtualiza.setString(2, contato.getSobrenome());
+        opAtualiza.setString(3, contato.getTelefone());
+        opAtualiza.setLong(4, contato.getId());
+        
+        
+        opAtualiza.executeUpdate();
+        
         }catch (SQLException ex){
             throw new Exception("Erro ao inserir o contato", ex);
         }
